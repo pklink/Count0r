b8e40dc14eaebbaa4abb33fab6662d97799d1462
Pierre Klink <dev@klinks.info>
2013-05-25 14:21:18 +0200

    added options to show & hide the buttons

--- Counter/build/source/r/debug/net/einself/countr/R.java
+++ Counter/build/source/r/debug/net/einself/countr/R.java
@@ -20,7 +20,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_visibility_both=0x7f080005;
+        public static final int action_visibility_only_minus=0x7f080007;
+        public static final int action_visibility_only_plus=0x7f080006;
         public static final int btnReset=0x7f080001;
         public static final int decrement=0x7f080004;
         public static final int increment=0x7f080002;
@@ -34,15 +36,17 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int btn_decrement=0x7f050002;
-        public static final int btn_increment=0x7f050003;
-        public static final int btn_reset=0x7f050004;
-        public static final int reset_confirm_negative=0x7f050005;
-        public static final int reset_confirm_positive=0x7f050006;
-        public static final int reset_confirm_title=0x7f050007;
-        public static final int txt_count_init_value=0x7f050008;
+        public static final int action_visibility_both=0x7f050000;
+        public static final int action_visibility_only_minus=0x7f050001;
+        public static final int action_visibility_only_plus=0x7f050002;
+        public static final int app_name=0x7f050003;
+        public static final int btn_decrement=0x7f050004;
+        public static final int btn_increment=0x7f050005;
+        public static final int btn_reset=0x7f050006;
+        public static final int reset_confirm_negative=0x7f050007;
+        public static final int reset_confirm_positive=0x7f050008;
+        public static final int reset_confirm_title=0x7f050009;
+        public static final int txt_count_init_value=0x7f05000a;
     }
     public static final class style {
         /** 
--- Counter/src/main/java/net/einself/countr/MainActivity.java
+++ Counter/src/main/java/net/einself/countr/MainActivity.java
@@ -1,20 +1,27 @@
 package net.einself.countr;
 
+import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     protected Item item;
 
     protected TextView counter;
+
+    private Button plus;
+
+    private Button minus;
 
 
     @Override
@@ -30,6 +37,10 @@ public class MainActivity extends Activity {
 
         // Text fuer Counter setzen
         counter.setText(item.getCount().toString());
+
+        // set buttons
+        plus  = (Button) findViewById(R.id.increment);
+        minus = (Button) findViewById(R.id.decrement);
 
         // Click-Event fuer Increment-Button erstellen
         ((Button) findViewById(R.id.increment)).setOnClickListener(new View.OnClickListener() {
@@ -82,5 +93,26 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        item.setChecked(true);
+
+        switch (item.getItemId()) {
+            case R.id.action_visibility_both:
+                plus.setVisibility(View.VISIBLE);
+                minus.setVisibility(View.VISIBLE);
+                return true;
+            case R.id.action_visibility_only_plus:
+                plus.setVisibility(View.VISIBLE);
+                minus.setVisibility(View.GONE);
+                return false;
+            case R.id.action_visibility_only_minus:
+                plus.setVisibility(View.GONE);
+                minus.setVisibility(View.VISIBLE);
+                return false;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
