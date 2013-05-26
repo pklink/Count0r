30932159e8aad08946e8d18c54874f04b8890716
Pierre Klink <dev@klinks.info>
2013-05-26 14:04:52 +0200

    counter formatting

--- Counter/src/main/java/net/einself/countr/MainActivity.java
+++ Counter/src/main/java/net/einself/countr/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -68,6 +70,7 @@ public class MainActivity extends Activity {
             }
         });
 
+
         // Click-Event fuer Decrement-Button erstellen
         getMinus().setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,7 +83,11 @@ public class MainActivity extends Activity {
             }
         });
 
+
         getCounter().addTextChangedListener(new TextWatcher() {
+
+            private boolean formatIsInProcess = false;
+
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
 
@@ -94,13 +101,28 @@ public class MainActivity extends Activity {
             @Override
             public void afterTextChanged(Editable editable) {
                 // get value
-                String value = getCounter().getText().toString();
+                String value = editable.toString().replace(" ", "");
 
-                if (value.length() == 0 || value.equals("-")) {
+                if (value.length() == 0 || "-".equals(value)) {
                     value = "0";
                 }
 
-                item.setCount(Long.parseLong(value));
+                if (!formatIsInProcess) {
+                    // flag formatting process as active
+                    formatIsInProcess = true;
+
+                    // format value
+                    value = NumberFormat.getInstance(Locale.US).format( Long.parseLong(value) ).replace(",", " ");
+
+                    // set formatted value in editable
+                    editable.clear();
+                    editable.append(value);
+
+                    // unflag formatting process as active
+                    formatIsInProcess = false;
+                }
+
+                item.setCount(Long.parseLong(value.replace(" ", "")));
             }
         });
     }
